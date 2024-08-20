4b4c523232b983b9c3947de22ea0b95dcf619fdf
Troy Kettle <89986560+Troy-Kettle@users.noreply.github.com>
2024-08-20 11:07:46 +0100

    Update IT2FLS-PaperRep.py

--- IT2FLS-PaperRep.py
+++ IT2FLS-PaperRep.py
@@ -36,53 +36,53 @@ def create_it2fs(universe, params, uncertainty=0.1):
 def define_fuzzy_sets(universes):
     fuzzy_sets = {
         'sbp': {
-            'Low +3': create_it2fs(universes['sbp'], [0, 0, 70, 75]),
-            'Low +2': create_it2fs(universes['sbp'], [70, 75, 80, 85]),
-            'Low +1': create_it2fs(universes['sbp'], [80, 85, 95, 100]),
-            'Normal +0': create_it2fs(universes['sbp'], [95, 100, 180, 185]),
+            'Low +3': create_it2fs(universes['sbp'], [0, 0, 70, 80]),
+            'Low +2': create_it2fs(universes['sbp'], [70, 80, 85, 90]),
+            'Low +1': create_it2fs(universes['sbp'], [85, 90, 100, 110]),
+            'Normal +0': create_it2fs(universes['sbp'], [100, 110, 180, 185]),
             'High +2': create_it2fs(universes['sbp'], [180, 185, 200, 200])
         },
         'hr': {
-            'Low +2': create_it2fs(universes['hr'], [0, 0, 45, 50]),
-            'Low +1': create_it2fs(universes['hr'], [45, 50, 55, 60]),
-            'Normal +0': create_it2fs(universes['hr'], [53, 60, 95, 100]),
-            'High +1': create_it2fs(universes['hr'], [95, 100, 105, 110]),
-            'High +2': create_it2fs(universes['hr'], [105, 110, 125, 130]),
+            'Low +2': create_it2fs(universes['hr'], [0, 0, 45, 55]),
+            'Low +1': create_it2fs(universes['hr'], [45, 55, 60, 65]),
+            'Normal +0': create_it2fs(universes['hr'], [60, 65, 85, 95]),
+            'High +1': create_it2fs(universes['hr'], [85, 95, 105, 115]),
+            'High +2': create_it2fs(universes['hr'], [105, 115, 125, 130]),
             'High +3': create_it2fs(universes['hr'], [125, 130, 140, 140])
         },
         'spo2': {
             'Low +3': create_it2fs(universes['spo2'], [80, 80, 83, 85]),
-            'Low +2': create_it2fs(universes['spo2'], [83, 85, 87, 90]),
-            'Low +1': create_it2fs(universes['spo2'], [87, 90, 92, 95]),
+            'Low +2': create_it2fs(universes['spo2'], [83, 85, 88, 90]),
+            'Low +1': create_it2fs(universes['spo2'], [88, 90, 93, 95]),
             'Normal +0': create_it2fs(universes['spo2'], [93, 95, 100, 100])
         },
         'temp': {
-            'Low +2': create_it2fs(universes['temp'], [35, 35, 36, 36.5]),
-            'Normal +0': create_it2fs(universes['temp'], [36, 36.5, 38, 38.5]),
-            'High +2': create_it2fs(universes['temp'], [38, 38.5, 40, 40])
+            'Low +2': create_it2fs(universes['temp'], [35, 35, 36, 37]),
+            'Normal +0': create_it2fs(universes['temp'], [36, 37, 38, 39]),
+            'High +2': create_it2fs(universes['temp'], [38, 39, 40, 40])
         },
         'bs': {
-            'Low +3': create_it2fs(universes['bs'], [60, 60, 63, 66]),
-            'Low +2': create_it2fs(universes['bs'], [63, 66, 70, 72]),
-            'Normal +0': create_it2fs(universes['bs'], [70, 72, 106, 110]),
-            'High +2': create_it2fs(universes['bs'], [106, 110, 140, 150]),
+            'Low +3': create_it2fs(universes['bs'], [60, 60, 63, 68]),
+            'Low +2': create_it2fs(universes['bs'], [63, 68, 75, 80]),
+            'Normal +0': create_it2fs(universes['bs'], [75, 80, 110, 115]),
+            'High +2': create_it2fs(universes['bs'], [110, 115, 140, 150]),
             'High +3': create_it2fs(universes['bs'], [140, 150, 160, 160])
         },
         'risk': {
-            'NRM': create_it2fs(universes['risk'], [0, 0, 0.5, 1.5]),
-            'LRG1': create_it2fs(universes['risk'], [0.5, 1.5, 1.5, 2.5]),
-            'LRG2': create_it2fs(universes['risk'], [1.5, 2.5, 2.5, 3.5]),
-            'LRG3': create_it2fs(universes['risk'], [2.5, 3.5, 3.5, 4.5]),
-            'LRG4': create_it2fs(universes['risk'], [3.5, 4.5, 4.5, 5.5]),
-            'HRG5': create_it2fs(universes['risk'], [4.5, 5.5, 5.5, 6.5]),
-            'HRG6': create_it2fs(universes['risk'], [5.5, 6.5, 6.5, 7.5]),
-            'HRG7': create_it2fs(universes['risk'], [6.5, 7.5, 7.5, 8.5]),
-            'HRG8': create_it2fs(universes['risk'], [7.5, 8.5, 8.5, 9.5]),
-            'HRG9': create_it2fs(universes['risk'], [8.5, 9.5, 9.5, 10.5]),
-            'HRG10': create_it2fs(universes['risk'], [9.5, 10.5, 10.5, 11.5]),
-            'HRG11': create_it2fs(universes['risk'], [10.5, 11.5, 11.5, 12.5]),
-            'HRG12': create_it2fs(universes['risk'], [11.5, 12.5, 12.5, 13.5]),
-            'HRG13': create_it2fs(universes['risk'], [12.5, 13.5, 13.5, 14]),
+            'NRM': create_it2fs(universes['risk'], [0, 0, 0.5, 1]),
+            'LRG1': create_it2fs(universes['risk'], [0.5, 1, 1.5, 2]),
+            'LRG2': create_it2fs(universes['risk'], [1.5, 2, 2.5, 3]),
+            'LRG3': create_it2fs(universes['risk'], [2.5, 3, 3.5, 4]),
+            'LRG4': create_it2fs(universes['risk'], [3.5, 4, 4.5, 5]),
+            'HRG5': create_it2fs(universes['risk'], [4.5, 5, 5.5, 6]),
+            'HRG6': create_it2fs(universes['risk'], [5.5, 6, 6.5, 7]),
+            'HRG7': create_it2fs(universes['risk'], [6.5, 7, 7.5, 8]),
+            'HRG8': create_it2fs(universes['risk'], [7.5, 8, 8.5, 9]),
+            'HRG9': create_it2fs(universes['risk'], [8.5, 9, 9.5, 10]),
+            'HRG10': create_it2fs(universes['risk'], [9.5, 10, 10.5, 11]),
+            'HRG11': create_it2fs(universes['risk'], [10.5, 11, 11.5, 12]),
+            'HRG12': create_it2fs(universes['risk'], [11.5, 12, 12.5, 13]),
+            'HRG13': create_it2fs(universes['risk'], [12.5, 13, 13.5, 14]),
             'HRG14': create_it2fs(universes['risk'], [13.5, 14, 14, 14])
         }
     }
@@ -120,12 +120,12 @@ def main():
     plot_it2fs(axs[0, 0], universes['sbp'], fuzzy_sets['sbp'], 'Systolic Blood Pressure (SBP)')
     plot_it2fs(axs[0, 1], universes['hr'], fuzzy_sets['hr'], 'Heart Rate (HR)')
     plot_it2fs(axs[1, 0], universes['spo2'], fuzzy_sets['spo2'], 'SPO2')
-    plot_it2fs(axs[1, 1], universes['temp'], fuzzy_sets['temp'], 'Temperature')
-    plot_it2fs(axs[2, 0], universes['bs'], fuzzy_sets['bs'], 'Blood Sugar')
+    plot_it2fs(axs[1, 1], universes['temp'], fuzzy_sets['temp'], 'Temperature (T)')
+    plot_it2fs(axs[2, 0], universes['bs'], fuzzy_sets['bs'], 'Blood Sugar (BS)')
     plot_it2fs(axs[2, 1], universes['risk'], fuzzy_sets['risk'], 'Risk')
 
     plt.tight_layout(rect=[0, 0, 0.9, 0.95])  # Adjust layout to accommodate the legend
     plt.show()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
